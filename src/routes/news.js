056f85ffbfbbea95bc15b8d1203544c4124b3a03
rohanh007 <77897898+rohanh007@users.noreply.github.com>
2023-09-13 22:41:34 +0530

    Update news.js

--- src/routes/news.js
+++ src/routes/news.js
@@ -61,18 +61,18 @@ newsRouter.post('', async(req, res) => {
     let search = req.body.search
     try {
         const newsAPI = await axios.get(`https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=d894282ca6504df5a115e9a5330af163&q=${search}`)
-        res.render('newsSearch', { articles : newsAPI.data.articles })
+        res.render('newssearch', { articles : newsAPI.data.articles })
     } catch (err) {
         if(err.response) {
-            res.render('newsSearch', { articles : null })
+            res.render('newssearch', { articles : null })
             console.log(err.response.data)
             console.log(err.response.status)
             console.log(err.response.headers)
         } else if(err.request) {
-            res.render('newsSearch', { articles : null })
+            res.render('newssearch', { articles : null })
             console.log(err.request)
         } else {
-            res.render('newsSearch', { articles : null })
+            res.render('newssearch', { articles : null })
             console.error('Error', err.message)
         }
     } 
